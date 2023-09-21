c20bcfba5566a581e090d451322f54b0934cc754
ural89 <uralkemal@gmail.com>
2023-09-21 21:56:53 +0300

    Perception

--- Source/TopDownShooter/Private/Fighter/FighterAIController.cpp
+++ Source/TopDownShooter/Private/Fighter/FighterAIController.cpp
@@ -2,20 +2,40 @@
 
 #include "Fighter/FighterAIController.h"
 #include "Fighter/FighterBase.h"
-
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 AFighterAIController::AFighterAIController()
 {
     PrimaryActorTick.bCanEverTick = true;
+    SetupPerception();
+}
+void AFighterAIController::SetupPerception()
+{
+    AISight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+    SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent")));
+    AISight->SightRadius = sightRadius;
+    AISight->LoseSightRadius = looseSightRadius;
+    AISight->PeripheralVisionAngleDegrees = 180;
+    AISight->DetectionByAffiliation.bDetectEnemies = true;
+    AISight->DetectionByAffiliation.bDetectFriendlies = true;
+    AISight->DetectionByAffiliation.bDetectNeutrals = true;
+    AISight->AutoSuccessRangeFromLastSeenLocation = 1500;
+    PerceptionComponent->ConfigureSense(*AISight);
+    PerceptionComponent->SetDominantSense(AISight->GetSenseImplementation());
+    PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AFighterAIController::OnPerceptionUpdated);
 }
 void AFighterAIController::OnPossess(APawn *InPawn)
 {
-
     Super::OnPossess(InPawn);
     OwnerFighter = Cast<AFighterBase>(InPawn);
+
     FTimerHandle CreatePathTimerHandle;
     GetWorld()->GetTimerManager().SetTimer(CreatePathTimerHandle, this, &AFighterAIController::UpdatePath, 0.1f, true);
 }
-
+void AFighterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+}
 void AFighterAIController::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
@@ -29,12 +49,22 @@ void AFighterAIController::UpdatePath()
 {
     FVector Destination = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
     MoveToLocation(Destination, stoppingRange, true, true);
+    UE_LOG(LogTemp, Warning, TEXT("MoveDirection %f, %f"), Destination.X, Destination.Y);
 }
 void AFighterAIController::MoveToDirection()
 {
     FVector MoveDirection = GetPathFollowingComponent()->GetCurrentDirection();
     OwnerFighter->MoveForward(MoveDirection.X);
     OwnerFighter->MoveRight(MoveDirection.Y);
+}
+
+void AFighterAIController::OnPerceptionUpdated(AActor *Actor, FAIStimulus Stimulus)
+{
+    if (Actor)
+    {
+        canMove = true;
+        UE_LOG(LogTemp, Warning, TEXT("Percieved Actor"));
+    }
 }
 void AFighterAIController::Stop()
 {
@@ -47,7 +77,7 @@ void AFighterAIController::OnMoveCompleted(FAIRequestID RequestID, const FPathFo
     Super::OnMoveCompleted(RequestID, Result);
     if (Result.IsSuccess())
     {
-        UE_LOG(LogTemp, Warning, TEXT("Path completed"));
+
         Stop();
     }
 }
--- Source/TopDownShooter/Public/Fighter/FighterAIController.h
+++ Source/TopDownShooter/Public/Fighter/FighterAIController.h
@@ -18,17 +18,31 @@ public:
 	AFighterAIController();
 	void Tick(float DeltaTime) override;
 	virtual void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult &Result) override;
+	virtual void BeginPlay() override;
 protected:
 	virtual void OnPossess(APawn *InPawn) override;
 
 private:
-	bool canMove = true;
+	bool canMove = false;
 	float stoppingRange = 200;
+
+	//Perception variables
+	float sightRadius = 600;
+	float looseSightRadius = 200;
 	
+
+
 	class AFighterBase* OwnerFighter;
 	
+	class UAISenseConfig_Sight* AISight;
+
+	void SetupPerception();
 	void UpdatePath();
 	void MoveToDirection();
+
+	UFUNCTION()
+	void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
 	void Stop();
 
 
